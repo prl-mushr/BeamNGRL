45afc117d258311deda251cd554bf2e712ae3f3e
sidtalia <sidharthtalia@ymail.com>
2023-10-09 13:22:24 -0700

    latest from HOUND

--- BeamNGRL/gym/envs/OffroadSmallIsland.py
+++ BeamNGRL/gym/envs/OffroadSmallIsland.py
@@ -6,86 +6,109 @@ from BeamNGRL.utils.planning import update_goal
 from gym import spaces
 
 class OffroadSmallIsland(gym.Env):
-    def __init__(self, config_path, waypoint_path,  Map_config, camera_config, lidar_config, IMU_config, args):        
-        self.target_WP = np.load(waypoint_path + "WP_file_offroad.npy")[:100]
-        with open(config_path + 'Map_config.yaml') as f:
-            Map_config = yaml.safe_load(f)
+    def __init__(self, hal_config_path = None, config_path=None, args=None):
+        if config_path is None:
+            print("no config file provided!")
+            exit()
+        if hal_config_path is None:
+            print("no hal config file provided!")
+            exit()
+
+        with open(config_path) as f:
+            Config = yaml.safe_load(f)
+        with open(hal_config_path) as f:
+            hal_Config = yaml.safe_load(f)
+
+        vehicle = Config["vehicle"]
+        start_pos = np.array(Config["start_pos"]) ## some default start position which will be overwritten by the scenario file
+        start_quat = np.array(Config["start_quat"])
+        Map_config = Config["Map_config"]
+        scenario = Config["scenario"] ## TODO: WP_file_offroad does not contain heading data
+
+        WP_file = str(Path(os.getcwd()).parent.absolute()) + "/Experiments/Waypoints/" + scenario + ".npy"
+        self.target_WP = np.load(WP_file)[:100] ## this was up to the first 100 points to prevent trying to perform the full loop.
 
         self.map_res = Map_config["map_res"]
         self.map_size = Map_config["map_size"]
         self.map_size_px = int(self.map_size/self.map_res)
-
-        self.bng_interface = get_beamng_default(
-            car_model="offroad",
-            start_pos=start_pos, ## start position in ENU (east north up). Center of the map is usually 0,0, height is terrain dependent. TODO: use the map model to estimate terrain height.
-            start_quat=start_quat, ## start quaternion -- TODO: there should be a ROS to BeamNG to ROS conversion system for reference frames.
-            car_make="sunburst", ## car make (company/manufacturer)
-            map_config=Map_config, ## Map config; this is "necessary"
-            remote=args.remote, ## are you running the simulator remotely (on a separate computer or on the same computer but outside the docker)? 
-            host_IP=args.host_IP, ## if using a remote connection (usually the case when running sim on a separate computer)
-            camera_config=camera_config, ## currently, camera only works on windows, so you can only use this if you have the sim running remotely or you're using windows as the host
-            lidar_config=lidar_config, ## currently, lidar only works on windows, so you can only use this if the sim is running remotely or you're using a windows host
-            accel_config=IMU_config, ## IMU config. if left blank, a default config is used.
-            burn_time=args.step_time, ## step or dt time
-            run_lockstep=True ## whether the simulator waits for control input to move forward in time. Set to true to have a gym "step" like functionality
-        )
         
         self.current_wp_index = 0  # initialize waypoint index with 0
         self.goal = None
         action = np.zeros(2)
         self.lookahead = 15
-        for i in range(10):
-            self.bng_interface.send_ctrl(action)
-            self.bng_interface.state_poll()
 
         action_low = np.array([-1.0, -1.0])  # Minimum speed and steering values
         action_high = np.array([1.0, 1.0])  # Maximum speed and steering values
         action_space = spaces.Box(low=action_low, high=action_high, shape=(2,), dtype=np.float64)
         self.action_space = action_space
-
+        ## TODO: add camera image to observation space?
         self.observation_space = spaces.Dict({
             'height': spaces.Box(low=-Map_config["elevation_range"], high=Map_config["elevation_range"], shape=(self.map_size_px, self.map_size_px, 1), dtype=np.float32),
-            'normal': spaces.Box(low=-1.0, high=1.0, shape=(self.map_size_px, self.map_size_px, 3), dtype=np.float32),
+            'normal': spaces.Box(low=-1.0, high=1.0, shape=(self.map_size_px, self.map_size_px, 3), dtype=np.float64),
             'color':  spaces.Box(low=0, high=255, shape=(self.map_size_px, self.map_size_px, 3), dtype=np.uint8),
-            'pos': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
-            'rpy': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
-            'lin_vel': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
-            'ang_vel': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
-            'accel': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32),
-            'last_action': spaces.Box(low=-1.0, high=1.0, shape=(2,), dtype=np.float32),
+            'pos': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float64),
+            'rpy': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float64),
+            'lin_vel': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float64),
+            'ang_vel': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float64),
+            'accel': spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float64),
+            'last_action': spaces.Box(low=-1.0, high=1.0, shape=(2,), dtype=np.float64),
         })
+        self.bng_interface = get_beamng_default(
+            car_model=vehicle["model"],
+            start_pos=start_pos,
+            start_quat=start_quat,
+            car_make=vehicle["make"],
+            map_config=Map_config,
+            host_IP=args.host_IP,
+            remote=args.remote,
+            camera_config=hal_Config["camera"],
+            lidar_config=hal_Config["lidar"],
+            accel_config=hal_Config["mavros"],
+            burn_time=Config["burn_time"],
+            run_lockstep=Config["run_lockstep"],
+        )
+        for i in range(10):
+            self.bng_interface.send_ctrl(action)
+            self.bng_interface.state_poll()
 
-    def reset(self, seed):
+    def reset(self):
         self.bng_interface.reset()
         obs, _, _, info = self.step(np.zeros(2,dtype=np.float64))
         return obs, info
 
     def step(self, action):
-        self.bng_interface.send_ctrl(action)
-        self.bng_interface.state_poll()
-        state = self.bng_interface.state
-        
-        observations = {
-            'height': self.bng_interface.BEV_heght,
-            'normal': self.bng_interface.BEV_normal,
-            'color':  self.bng_interface.BEV_color,
-            'pos': state[:3],
-            'rpy': state[3:6],
-            'lin_vel': state[6:9],
-            'ang_vel': state[9:12],
-            'accel': state[12:15],
-            'last_action': state[15:17],
-        }
+        try:
+            self.bng_interface.send_ctrl(action)
+            self.bng_interface.state_poll() ## this steps the environment forward (it is the equivalent of doing a "step")
+            state = self.bng_interface.state
+            print(self.bng_interface.BEV_heght.dtype, self.bng_interface.BEV_normal.dtype, self.bng_interface.BEV_color.dtype, state.dtype)
+            observations = {
+                'height': self.bng_interface.BEV_heght,
+                'normal': self.bng_interface.BEV_normal,
+                'color':  self.bng_interface.BEV_color,
+                'pos': state[:3],
+                'rpy': state[3:6],
+                'lin_vel': state[6:9],
+                'ang_vel': state[9:12],
+                'accel': state[12:15],
+                'last_action': state[15:17],
+            }
 
-        pos = np.copy(state[:2])  # example of how to get car position in world frame. All data points except for dt are 3 dimensional.
-        goal, done, self.current_wp_index = update_goal(
-            self.goal, pos, self.target_WP, self.current_wp_index, self.lookahead, step_size=10
-        )
+            pos = np.copy(state[:2])
+            goal, done, self.current_wp_index = update_goal(
+                self.goal, pos, self.target_WP, self.current_wp_index, self.lookahead, step_size=10
+            )
 
-        damage = len(self.bng_interface.broken)
+            damage = len(self.bng_interface.broken)
 
-        reward = np.linalg.norm(goal - pos) + 100.0 * damage
+            reward = np.linalg.norm(goal - pos) + 100.0 * damage
 
-        info = dict({'timestamp': self.bng_interface.timestamp})
+            info = dict({'timestamp': self.bng_interface.timestamp})
 
-        return observations, reward, done, info
+            return observations, reward, done, info
+
+        except KeyboardInterrupt:
+            self.bng_interface.bng.close()
+            os._exit(1)
+        except Exception as e:
+            print(e)
--- examples/gym_example.py
+++ examples/gym_example.py
@@ -3,30 +3,22 @@ import gym
 import BeamNGRL.gym
 from pathlib import Path
 import os
+import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--hal_config_name", type=str, default="offroad.yaml", help="name of the HAL config file to use")
-parser.add_argument("--remote", type=bool, default=False, help="whether to connect to a remote beamng server")
+parser.add_argument("--config_name", type=str, default="gym_experiment.yaml", help="name of the config used for experiment specifics")
+parser.add_argument("--remote", type=bool, default=True, help="whether to connect to a remote beamng server")
 parser.add_argument("--host_IP", type=str, default="169.254.216.9", help="host ip address if using remote beamng")
 
 args = parser.parse_args()
 hal_config_name = args.hal_config_name
 hal_config_path = str(Path(os.getcwd()).parent.absolute()) + "/Configs/" + hal_config_name
-with torch.no_grad():
-    main(config_path = config_path, hal_config_path = hal_config_path, args = args) ## we run for 3 iterations because science
+config_name = args.config_name
+config_path = str(Path(os.getcwd()).parent.absolute()) + "/Experiments/Configs/" +  config_name ## anything experiment specific should be put here
 
-waypoint_path = str(Path(os.getcwd()).parent.absolute()) + "/BeamNGRL/utils/waypoint_files/" ## this should come from test-specific file!
-camera_config = dict()
-lidar_config = dict()
-IMU_config = dict()
-Map_config = dict()
-
-env = gym.make('offroad-small-island-v0', config_path=config_path, waypoint_path=waypoint_path, Map_config=Map_config, camera_config=camera_config, lidar_config=lidar_config, IMU_config=IMU_config, args=args)
-## TODO: fix gym backend.
-## TODO: load configs from Config folder.
-## TODO: add the parser shit
-
-## TODO: move minimal example to this directory
+env = gym.make('offroad-small-island-v0', hal_config_path=hal_config_path, config_path=config_path, args=args)
+## env.config would be the experiment config dict
 env.reset()
 total_reward, done = 0, False
 # Drive around randomly until finishing
