53ae6ccd204739afb0b573cd24064daa0963c7d9
sidtalia <sidharthtalia@ymail.com>
2025-06-17 17:36:54 -0700

    commits from field testing

--- BeamNGRL/control/UW_mppi/Costs/TrackingCarCost.py
+++ BeamNGRL/control/UW_mppi/Costs/TrackingCarCost.py
@@ -17,7 +17,7 @@ class SimpleCarCost(torch.nn.Module):
         self.d = device
 
         # self.critical_SA = torch.tensor(Cost_config["critical_SA"], dtype=self.dtype, device=self.d)
-        # self.speed_target = torch.tensor(Cost_config["speed_target"], dtype=self.dtype, device=self.d)
+        self.throttle_threshold = torch.tensor(Cost_config["throttle_threshold"], dtype=self.dtype, device=self.d)
         self.critical_RI = torch.tensor(Cost_config["critical_RI"], dtype=self.dtype, device=self.d)
         self.lethal_w = torch.tensor(Cost_config["lethal_w"], dtype=self.dtype, device=self.d)
         # self.stop_w   = torch.tensor(Cost_config["stop_w"], dtype=self.dtype, device=self.d)
@@ -90,47 +90,45 @@ class SimpleCarCost(torch.nn.Module):
         wx = state[...,12]
         wy = state[...,13]
         wz = state[...,14]
-                
+        th = state[...,16]
         img_X = self.meters_to_px(x)
         img_Y = self.meters_to_px(y)
-        
-        beta = torch.atan2(vy, vx)
+        beta = torch.atan2(vy, vx)**2
         # cy = torch.cos(beta)
         # sy = torch.sin(beta)
         cy = torch.cos(yaw)
         sy = torch.sin(yaw)
         V = torch.sqrt(vx**2 + vy**2) * torch.sign(vx)
-        flx = x + self.car_l2*cy - self.car_w2*sy
-        fly = y + self.car_l2*sy + self.car_w2*cy
-        frx = x + self.car_l2*cy + self.car_w2*sy
-        fry = y + self.car_l2*sy - self.car_w2*cy
-        blx = x - self.car_l2*cy - self.car_w2*sy
-        bly = y - self.car_l2*sy + self.car_w2*cy
-        brx = x - self.car_l2*cy + self.car_w2*sy
-        bry = y - self.car_l2*sy - self.car_w2*cy
+        # flx = x + self.car_l2*cy - self.car_w2*sy
+        # fly = y + self.car_l2*sy + self.car_w2*cy
+        # frx = x + self.car_l2*cy + self.car_w2*sy
+        # fry = y + self.car_l2*sy - self.car_w2*cy
+        # blx = x - self.car_l2*cy - self.car_w2*sy
+        # bly = y - self.car_l2*sy + self.car_w2*cy
+        # brx = x - self.car_l2*cy + self.car_w2*sy
+        # bry = y - self.car_l2*sy - self.car_w2*cy
 
-        flx_px = self.meters_to_px(flx)
-        fly_px = self.meters_to_px(fly)
-        frx_px = self.meters_to_px(frx)
-        fry_px = self.meters_to_px(fry)
-        blx_px = self.meters_to_px(blx)
-        bly_px = self.meters_to_px(bly)
-        brx_px = self.meters_to_px(brx)
-        bry_px = self.meters_to_px(bry)
+        # flx_px = self.meters_to_px(flx)
+        # fly_px = self.meters_to_px(fly)
+        # frx_px = self.meters_to_px(frx)
+        # fry_px = self.meters_to_px(fry)
+        # blx_px = self.meters_to_px(blx)
+        # bly_px = self.meters_to_px(bly)
+        # brx_px = self.meters_to_px(brx)
+        # bry_px = self.meters_to_px(bry)
         
-        # # state_cost = state_cost + torch.clamp(torch.square(self.BEVmap_height[img_Y, img_X]) - 0.09, 0, 10)
-        # # state_cost = torch.square(self.BEVmap_cost[img_Y, img_X,0])
-        # # evaluate state cost using footprint
-        # # state cost is the maximum state cost of all the footprint points
-        state_cost = torch.zeros_like(x)
-        state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[fly_px, flx_px]))
-        state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[fry_px, frx_px]))
-        state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[bly_px, blx_px]))
-        state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[bry_px, brx_px]))
+        # # # state_cost = state_cost + torch.clamp(torch.square(self.BEVmap_height[img_Y, img_X]) - 0.09, 0, 10)
+        state_cost = torch.square(self.BEVmap_cost[img_Y, img_X]/255.0)
+        # # # evaluate state cost using footprint
+        # # # state cost is the maximum state cost of all the footprint points
+        # state_cost = torch.zeros_like(x)
+        # state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[fly_px, flx_px]))
+        # state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[fry_px, frx_px]))
+        # state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[bly_px, blx_px]))
+        # state_cost = torch.max(state_cost, torch.square(self.BEVmap_cost[bry_px, brx_px]))
 
         cr = torch.cos(roll)
         cp = torch.cos(pitch)
-        beta = torch.atan2(vy, vx)**2
         # now we compute running cost as a weighted sum of the xy, cy-sy, and velocity errors
         roll_ditch_cost = (
             + torch.clamp(torch.abs(az - self.GRAVITY*cr*cp) - self.critical_vert_acc, 0, 10.0)/10.0
@@ -145,23 +143,23 @@ class SimpleCarCost(torch.nn.Module):
         sy_err = sy - torch.sin(self.path[:,2])
         yaw_err = cy_err**2 + sy_err**2
         # velocity error
-        vel_err = (V - self.path[:,3])**2
+        vel_err = torch.clamp(V - self.throttle_threshold, 0, 5)**2
 
         pos_err = x_err**2 + y_err**2
         running_cost = self.pos_w * pos_err + self.heading_w * yaw_err + self.speed_w * vel_err + beta*1.5
         if self.scaling is None:
             steps = running_cost.shape[-1]
             self.scaling = torch.linspace(0.1, self.scaling_factor, steps, device=self.d)
-        running_cost = running_cost * self.scaling + roll_ditch_cost
-        constraint_cost[torch.where(pos_err < 1)] = 0
-        # roll_ditch_cost[torch.where(pos_err < 1)] = 0
+        running_cost = running_cost * self.scaling + roll_ditch_cost# + constraint_cost
+        # constraint_cost[torch.where(pos_err < 1)] = 0
+        # roll_ditch_cost[torch.where(pos_err < 0.25)] = 0
 
         constraint_cost = constraint_cost.mean(dim=0).sum(dim=1)
-        if torch.all(constraint_cost > self.lethal_w):
+        if torch.all(roll_ditch_cost > self.lethal_w):# or torch.all(constraint_cost > self.lethal_w):
             self.bad_physics = True
         else:
             self.bad_physics = False
 
         ## for running cost mean over the 0th dimension (bins), which results in a KxT tensor. Then sum over the 1st dimension (time), which results in a [K] tensor.
         ## for terminal cost, just mean over the 0th dimension (bins), which results in a [K] tensor.
-        return (running_cost.mean(dim=0)).sum(dim=1) + constraint_cost
+        return (running_cost.mean(dim=0)).sum(dim=1)
